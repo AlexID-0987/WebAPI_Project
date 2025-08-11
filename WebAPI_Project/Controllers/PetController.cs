e147aae851985b00938ecafdbb97c5a9b22ec4b7
alex <alex@DESKTOP-E2FBF73>
2025-08-11 21:45:01 +0300

    modify my code

--- WebAPI_Project/Controllers/PetController.cs
+++ WebAPI_Project/Controllers/PetController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using WebAPI_Project.Models;
 
 namespace WebAPI_Project.Controllers
@@ -24,13 +25,49 @@ namespace WebAPI_Project.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Pet>> GettAll(int id)
         {
-            Pet?  pet= await _petDbContext.Pets.FirstOrDefaultAsync(x => x.Id == id);
-            if(pet==null)
+            Pet? pet = await _petDbContext.Pets.FirstOrDefaultAsync(x => x.Id == id);
+            if (pet == null)
             {
                 return NotFound();
             }
             return pet;
 
+        }
+        [HttpPost]
+        public async Task<ActionResult<Pet>>Post(Pet Pet)
+        {
+            await _petDbContext.Pets.AddAsync(Pet);
+            await _petDbContext.SaveChangesAsync();
+            if(Pet.Id == 0)
+            {
+                return BadRequest("Pet could not be created.");
+            }
+            return Pet;
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Pet>> Put(int id, Pet Pet)
+        {
+            if (id != Pet.Id)
+            {
+                return BadRequest("Pet ID mismatch.");
+            }
+            _petDbContext.Entry(Pet).State = EntityState.Modified;
+            try
+            {
+                await _petDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_petDbContext.Pets.Any(x => x.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Pet;
         }
     }
 }
